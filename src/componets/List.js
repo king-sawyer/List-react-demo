95cce3039ba9772cef240f0a6ca6a412d6eb1ceb
Sawyer King <kingsawyer555@gmail.com>
2020-09-15 07:59:14 -0700

    added delete and random card buttons

--- src/componets/List.js
+++ src/componets/List.js
@@ -11,9 +11,19 @@ function List(props) {
       <header className="List-header">{props.header}</header>
       <div className="List-cards">
         {props.cards.map((card) => (
-          <Card key={card.id} title={card.title} content={card.content} />
+          <Card
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            content={card.content}
+            handleDelete={props.handleDelete}
+          />
         ))}
-        <button type="button" className="List-add-button">
+        <button
+          type="button"
+          className="List-add-button"
+          onClick={() => props.handleRandomCard(props.id)}
+        >
           + Add Random Card
         </button>
       </div>
